424f5d833afe970043dc0236544e27f060af3594
Brad Langhorst <langhorst@neb.com>
2013-07-29 23:05:53 -0400

    write barcodes and multiplexed params where theyre expected

--- bcl2fastq.sh
+++ bcl2fastq.sh
@@ -22,11 +22,11 @@ if [ ! -d ${run_path}/fastq ] ; then
 	mkdir ${run_path}/fastq
 fi
 
-echo 'barcode_sequence_1	barcode_name	library_name' > barcode_params.txt
-perl -nle 'print "$3\t$2\t$1" if /^([^,]+)(?:[^,]*,){4}([^,]+),([GCAT]+),.*$/' "${sample_sheet}" >> barcode_params.txt
-echo 'OUTPUT_PREFIX	BARCODE_1' > multiplex_params.txt
-echo 'unassigned	N' >> multiplex_params.txt
-perl -nle 'print "$1\t$3" if /^([^,]+)(?:[^,]*,){4}([^,]+),([GCAT]+),.*$/' "${sample_sheet}" >> multiplex_params.txt
+echo 'barcode_sequence_1	barcode_name	library_name' > ${run_path}/barcode_params.txt
+perl -nle 'print "$3\t$2\t$1" if /^([^,]+)(?:[^,]*,){4}([^,]+),([GCAT]+),.*$/' "${sample_sheet}" >> ${run_path}/barcode_params.txt
+echo 'OUTPUT_PREFIX	BARCODE_1' > ${run_path}/multiplex_params.txt
+echo 'unassigned	N' >> ${run_path}/multiplex_params.txt
+perl -nle 'print "$1\t$3" if /^([^,]+)(?:[^,]*,){4}([^,]+),([GCAT]+),.*$/' "${sample_sheet}" >> ${run_path}/multiplex_params.txt
 
 cd "${run_path}/fastq"
 java  $JAVA_OPTS -jar $PICARD_PATH/ExtractIlluminaBarcodes.jar  NUM_PROCESSORS=$CPU_COUNT READ_STRUCTURE=$READ_STRUCTURE LANE=001 BASECALLS_DIR=${run_path}/Data/Intensities/BaseCalls METRICS_FILE=barcode_metrics.txt BARCODE_FILE=${run_path}/barcode_params.txt
