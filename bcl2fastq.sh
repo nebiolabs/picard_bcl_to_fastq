641c8c0e1e846dab93cde89ed09341b29231c7a3
Zulchm <zulchm@student.elms.edu>
2016-07-27 17:01:14 -0400

    12 tiles 2 slots WAHOOOOOO!!!!

--- bcl2fastq.sh
+++ bcl2fastq.sh
@@ -202,13 +202,13 @@ do
         pushd "${output_path}/fastq/L_${i}_${FIRST_TILE}"
 
 
-	qsub -hold_jid lanebarcode${i} -N TileProcess -m abe -M ${2} -b y -pe smp 8 -cwd  $JAVA_PATH/java $JAVA_OPTS -jar $PICARD_PATH/picard.jar IlluminaBasecallsToFastq \
+	qsub -hold_jid lanebarcode${i} -N TileProcess -m abe -M ${2} -b y -pe smp 2 -cwd  $JAVA_PATH/java $JAVA_OPTS -jar $PICARD_PATH/picard.jar IlluminaBasecallsToFastq \
 		NUM_PROCESSORS=8 \
 		read_structure=$read_structure \
 		RUN_BARCODE=$run_barcode \
 		LANE=${i} \
 		FIRST_TILE= $FIRST_TILE \
-		TILE_LIMIT=6 \
+		TILE_LIMIT=12 \
 		MACHINE_NAME=$machine_name \
 		FLOWCELL_BARCODE=$flowcell \
 		BASECALLS_DIR="${run_path}/Data/Intensities/BaseCalls" \
